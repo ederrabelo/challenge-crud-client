0458e45aca2f55a4a6606cb54f58be745e76bfc4
Eder Afonso Rabelo <eafonsorabelo@gmail.com>
2025-07-11 18:45:10 -0400

    Initial commit

---
+++ src/main/java/com/ederrabelo/challenge_crud_client/ChallengeCrudClientApplication.java
@@ -0,0 +1,13 @@
+package com.ederrabelo.challenge_crud_client;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChallengeCrudClientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChallengeCrudClientApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ederrabelo/challenge_crud_client/ChallengeCrudClientApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ederrabelo.challenge_crud_client;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChallengeCrudClientApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
